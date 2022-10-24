d34b1376764373a8ac159bd1d33cfcfa0d47bac4
Grzesiek <g.pasko@gmail.com>
2022-10-24 17:42:38 +0200

    Refactoring

--- LongestSubsequence/Program.cs
+++ LongestSubsequence/Program.cs
@@ -3,14 +3,39 @@
 var random = new Random();
 
 var arr = Enumerable.Range(0, 200000).Select(x => random.Next(0, 50000)).Distinct().ToArray();
-LongestSubsequence(arr);
+var longest1 = LongestSubsequence(arr);
+PrintDetails(longest1, arr);
 
 arr = new[] {4, 6, 1, 3, 2, 4, 5, 100, 200, 1, 101, 105, 300, 102, 5, 104, 19, 103};
-LongestSubsequence(arr);
+var longest2 = LongestSubsequence(arr);
+PrintDetails(longest2, arr);
 
 Console.Read();
 
-void LongestSubsequence(int[] array)
+
+static void PrintDetails(Dictionary<int, int> longestSets, int[] input)
+{
+    if (!longestSets.Any())
+    {
+        Write("[No sequence found!]", ConsoleColor.Yellow);
+        return;
+    }
+
+    var longest = longestSets.OrderByDescending(x => x.Value).ThenBy(x => x.Key).First();
+    var start = input[longest.Key - longest.Value];
+    var sequence = string.Join(",", Enumerable.Range(start, longest.Value + 1));
+
+    Write($"Znaleziono {longestSets.Count()} ciągów", ConsoleColor.Green);
+    
+    Write($"Najdłuższy ciąg ma długość [{longest.Value + 1}] i jest to [{sequence}]", ConsoleColor.Cyan);
+    
+    var otherWithSameLength = longestSets.Count(x => x.Value == longest.Value);
+    
+    if(otherWithSameLength > 1) 
+        Write($"Istnieje więcej ([{otherWithSameLength}]) ciągów o tej samej długości", ConsoleColor.Yellow);
+}
+
+Dictionary<int,int> LongestSubsequence(int[] array)
 {
     array = array.Distinct().ToArray();
     
@@ -35,25 +60,7 @@ void LongestSubsequence(int[] array)
         }
     }
 
-    if (!longestSets.Any())
-    {
-        Write("[No sequence found!]", ConsoleColor.Yellow);
-        return;
-    }
-
-    var longest = longestSets.OrderByDescending(x => x.Value).ThenBy(x => x.Key).First();
-    var start = array[longest.Key - longest.Value];
-    var sequence = string.Join(",", Enumerable.Range(start, longest.Value + 1));
-
-    Write($"Znaleziono {longestSets.Count()} ciągów", ConsoleColor.Green);
-    
-    Write($"Najdłuższy ciąg ma długość [{longest.Value + 1}] i jest to [{sequence}]", ConsoleColor.Cyan);
-    
-    var otherWithSameLength = longestSets.Count(x => x.Value == longest.Value);
-    
-    if(otherWithSameLength > 1) 
-        Write($"Istnieje więcej ([{otherWithSameLength}]) ciągów o tej samej długości", ConsoleColor.Yellow);
-    
+    return longestSets;
 }
 
 static void Write(string message, ConsoleColor color)
